9af4e75caaf90bf1c45d8e974036a0c0a0b61869
aman <amankumarshrivas@gmail.com>
2019-04-15 06:59:09 +0530

    pdf facade addded

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -10,6 +10,7 @@ use App\Mentordetail;
 use User;
 use App\Cgpa;
 use Session;
+use  PDF;
 
 class FormController extends Controller
 {
@@ -125,6 +126,10 @@ class FormController extends Controller
     public function edit($id)
     {
         //
+        $user = Auth::user();
+        $post=Post::find($id);
+        $pdf = PDF::loadView('forms.pdf',compact('post'));
+          return $pdf->download('invoice.pdf');
     }
 
     /**
