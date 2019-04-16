c0d71db6e3765a7e5d11ccf04ef4dac6c1f78d57
aman <amankumarshrivas@gmail.com>
2019-04-16 20:04:14 +0530

    laravel title update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Mentorat') }}</title>
+    <title>Mentorat</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
